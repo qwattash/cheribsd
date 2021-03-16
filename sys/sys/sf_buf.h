2e75dbfb19fbeee1cb8b3b02bf5af764d32eda9f
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2021-03-16 13:11:43 +0000

    cheri: Update sf_buf implementation for CHERI
    - Use vm_pointer_t to store the sf buffer kva.
    - Set bounds on the mapped sf buffer kva.

--- sys/sys/sf_buf.h
+++ sys/sys/sf_buf.h
@@ -91,7 +91,7 @@ struct sf_buf {
 	LIST_ENTRY(sf_buf)	list_entry;	/* list of buffers */
 	TAILQ_ENTRY(sf_buf)	free_entry;	/* list of buffers */
 	vm_page_t		m;		/* currently mapped page */
-	vm_offset_t		kva;		/* va of mapping */
+	vm_pointer_t		kva;		/* va of mapping */
 	int			ref_count;	/* usage of this mapping */
 #if defined(SMP) && defined(SFBUF_CPUSET)
 	cpuset_t		cpumask;	/* where mapping is valid */
@@ -110,11 +110,11 @@ struct sf_buf *sf_buf_alloc(struct vm_page *, int);
 void sf_buf_free(struct sf_buf *);
 void sf_buf_ref(struct sf_buf *);
 
-static inline vm_offset_t
+static inline vm_pointer_t
 sf_buf_kva(struct sf_buf *sf)
 {
 	if (PMAP_HAS_DMAP)
-		return (PHYS_TO_DMAP(VM_PAGE_TO_PHYS((vm_page_t)sf)));
+		return (PHYS_TO_DMAP_PAGE(VM_PAGE_TO_PHYS((vm_page_t)sf)));
 
         return (sf->kva);
 }
