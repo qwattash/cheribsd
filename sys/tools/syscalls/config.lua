c4fd1fc76e37e02a38c491f27c38ee4cd7515856
Jessica Clarke <jrtc27@jrtc27.com>
2025-02-26 22:30:42 +0000

    sys/tools/syscalls: Support a separate abi_uintptr_t
    Currently the code assumes that the substitution for uintptr_t is the
    same as for intptr_t prefixed with a "u".  However, we'd like to support
    per-ABI type name prefixes.
    
    Introduce a separate abi_uintptr_t config key in order to support this.
    If not set, it will default to abi_intptr_t with a "u" prefix.
    
    Effort:		CHERI upstreaming
    Sponsored by:	Innovate UK

--- sys/tools/syscalls/config.lua
+++ sys/tools/syscalls/config.lua
@@ -45,6 +45,7 @@ local config = {
 	abi_ptr_array_t = "",
 	abi_headers = "",
 	abi_intptr_t = "intptr_t",
+	abi_uintptr_t = "uintptr_t",
 	ptr_intptr_t_cast = "intptr_t",
 	obsol = {},
 	unimpl = {},
@@ -230,6 +231,10 @@ function config.merge(fh)
 				config[k] = util.setFromString(v, "[^ ]+")
 			else
 				config[k] = v
+				if k:find("abi_intptr_t") and
+				    not config.modifications["abi_uintptr_t"] then
+					config["abi_uintptr_t"] = "u" .. v
+				end
 			end
 			-- Construct config modified table as config
 			-- is processed.
