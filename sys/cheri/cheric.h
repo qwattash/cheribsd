a87ef61cc53061be4c21a1e156dc1cae6741b7ba
Brooks Davis <brooks@FreeBSD.org>
2025-12-17 15:13:31 +0000

    cheri/cheric.h: add finger friendly CHERI C wrappers
    These cheri_ prefixed wrappers provide access to compiler intrinsics
    used to manipulate capabilities in ways not directly supported by
    the C language.  In userspace most come from cheriintrin.h which is
    shipped with CHERI-aware Clang compilers.  In the kernel, this file
    isn't reliably available so we duplicate the ones we need.
    
    Co-authored-by:	Robert Watson <rwatson@FreeBSD.org>
    Effort:		CHERI upstreaming
    Sponsored by:	Innovate UK

---
+++ sys/cheri/cheric.h
@@ -0,0 +1,385 @@
+/*-
+ * Copyright (c) 2013-2016 Robert N. M. Watson
+ * Copyright (c) 2021 Microsoft Corp.
+ * All rights reserved.
+ *
+ * This software was developed by SRI International and the University of
+ * Cambridge Computer Laboratory under DARPA/AFRL contract (FA8750-10-C-0237)
+ * ("CTSRD"), as part of the DARPA CRASH research programme.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef _SYS_CHERIC_H_
+#define	_SYS_CHERIC_H_
+
+#include <sys/cdefs.h>
+#include <sys/stddef.h>
+#include <sys/types.h>
+
+#if __has_feature(capabilities)
+#if !defined(_KERNEL) && !defined(_STANDALONE)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wignored-attributes"
+#include <cheriintrin.h>
+#pragma GCC diagnostic pop
+#include <stdbool.h>
+#endif
+
+#include <cheri/cherireg.h>	/* Permission definitions. */
+
+#if defined(_KERNEL) || defined(_STANDALONE)
+/*
+ * Programmer-friendly macros for CHERI-aware C code.  Mirrors
+ * cheriintrin.h from Clang.
+ */
+#define	cheri_flags_get(x)	__builtin_cheri_flags_get(x)
+#define	cheri_flags_set(x, y)	__builtin_cheri_flags_set((x), (y))
+#define cheri_address_get(x)	__builtin_cheri_address_get(x)
+#define cheri_address_set(x, y)	__builtin_cheri_address_set((x), (y))
+#define cheri_base_get(x)	__builtin_cheri_base_get(x)
+#define cheri_bounds_get(x)	__builtin_cheri_bounds_get(x)
+#define cheri_bounds_set(x, y) __builtin_cheri_bounds_set((x), (y))
+#define cheri_bounds_set_exact(x, y)					\
+    __builtin_cheri_bounds_set_exact((x), (y))
+#define cheri_cap_build(x, y)	__builtin_cheri_cap_build((x), (y))
+#define cheri_ddc_get()		__builtin_cheri_global_data_get()
+#define cheri_high_get(x)	__builtin_cheri_high_get(x)
+#define cheri_high_set(x, y)	__builtin_cheri_high_set((x), (y))
+#define cheri_is_equal_exact(x, y)					\
+    __builtin_cheri_equal_exact((x), (y))
+#define cheri_is_invalid(x)	(!__builtin_cheri_tag_get(x))
+#define cheri_is_sealed(x)	__builtin_cheri_sealed_get(x)
+#define cheri_is_sentry(x)						\
+    (__builtin_cheri_type_get(x) == CHERI_OTYPE_SENTRY)
+#define cheri_is_unsealed(x)	(!__builtin_cheri_sealed_get(x))
+#define cheri_is_valid(x)	__builtin_cheri_tag_get(x)
+#define cheri_length_get(x)	__builtin_cheri_length_get(x)
+#define cheri_offset_get(x)	__builtin_cheri_offset_get(x)
+#define cheri_offset_set(x, y)	__builtin_cheri_offset_set((x), (y))
+#define cheri_pcc_get()		__builtin_cheri_program_counter_get()
+#define cheri_perms_and(x, y)						\
+    __builtin_cheri_perms_and((x), (__SIZE_TYPE__)(y))
+#define cheri_perms_clear(x, y)						\
+    __builtin_cheri_perms_and((x), ~(__SIZE_TYPE__)(y))
+#define cheri_perms_get(x)	__builtin_cheri_perms_get(x)
+#define cheri_seal(x, y)	__builtin_cheri_seal((x), (y))
+#define cheri_seal_conditionally(x, y)				 	\
+    __builtin_cheri_conditional_seal((x), (y))
+#define cheri_sentry_create(x)	__builtin_cheri_seal_entry(x)
+#define cheri_tag_clear(x)	__builtin_cheri_tag_clear(x)
+#define cheri_tag_get(x)	__builtin_cheri_tag_get(x)
+#define cheri_type_copy(x, y)	__builtin_cheri_cap_type_copy((x), (y))
+#define cheri_type_get(x)	__builtin_cheri_type_get(x)
+#define cheri_unseal(x, y)	__builtin_cheri_unseal((x), (y))
+
+#endif /* defined(_KERNEL) || defined(_STANDALONE) */
+
+#define __WANT_OLD_CHERI_MACROS
+#ifdef __WANT_OLD_CHERI_MACROS
+#define	cheri_getlen(x)		__builtin_cheri_length_get((x))
+#define	cheri_getlength(x)	__builtin_cheri_length_get((x))
+#define	cheri_getbase(x)	__builtin_cheri_base_get((x))
+#define	cheri_getoffset(x)	__builtin_cheri_offset_get((x))
+#define	cheri_getaddress(x)	__builtin_cheri_address_get((x))
+#define	cheri_getflags(x)	__builtin_cheri_flags_get((x))
+#define	cheri_getperm(x)	__builtin_cheri_perms_get((x))
+#define	cheri_getsealed(x)	__builtin_cheri_sealed_get((x))
+#define	cheri_gettag(x)		__builtin_cheri_tag_get((x))
+#define	cheri_gettype(x)	((long)__builtin_cheri_type_get((x)))
+
+#define	cheri_andperm(x, y)	__builtin_cheri_perms_and((x), (y))
+#define	cheri_clearperm(x, y)	__builtin_cheri_perms_and((x), ~(y))
+#define	cheri_cleartag(x)	__builtin_cheri_tag_clear((x))
+#define	cheri_incoffset(x, y)	__builtin_cheri_offset_increment((x), (y))
+#define	cheri_setoffset(x, y)	__builtin_cheri_offset_set((x), (y))
+#define	cheri_setaddress(x, y)	__builtin_cheri_address_set((x), (y))
+#define	cheri_setflags(x, y)	__builtin_cheri_flags_set((x), (y))
+
+#define	cheri_buildcap(x, y)	__builtin_cheri_cap_build((x), (y))
+
+#define	cheri_copytype(x, y)	__builtin_cheri_cap_type_copy((x), (y))
+
+#define	cheri_seal(x, y)	__builtin_cheri_seal((x), (y))
+#define	cheri_unseal(x, y)	__builtin_cheri_unseal((x), (y))
+#define	cheri_sealentry(x)	__builtin_cheri_seal_entry((x))
+#define	cheri_condseal(x, y)	__builtin_cheri_conditional_seal((x), (y))
+
+#define	cheri_ccheckperm(c, p)	__builtin_cheri_perms_check((c), (p))
+#define	cheri_cchecktype(c, t)	__builtin_cheri_type_check((c), (t))
+
+#define	cheri_getdefault()	__builtin_cheri_global_data_get()
+#define	cheri_getpcc()		__builtin_cheri_program_counter_get()
+#define	cheri_getstack()	__builtin_cheri_stack_get()
+
+#define	cheri_local(c)		cheri_andperm((c), ~CHERI_PERM_GLOBAL)
+
+#define	cheri_setbounds(x, y)	__builtin_cheri_bounds_set((x), (y))
+#define	cheri_setboundsexact(x, y)	__builtin_cheri_bounds_set_exact((x), (y))
+
+/* Compare capabilities including bounds and perms etc. */
+#define cheri_equal_exact(x, y) __builtin_cheri_equal_exact(x, y)
+
+#endif /* __WANT_OLD_CHERI_MACROS */
+
+#define	cheri_loadtags(m)	__builtin_cheri_cap_load_tags((m))
+
+/*
+ * Soft implementation of cheri_subset_test().
+ * Test whether a capability is a subset of another.
+ * NOTE: This is to be replaced by LLVM intrinsic once the intrinsic and
+ * related instruction arguments are stable.
+ */
+#undef cheri_is_subset
+#define        cheri_is_subset(parent, ptr)				\
+	(cheri_tag_get(parent) == cheri_tag_get(ptr) &&			\
+	 cheri_base_get(ptr) >= cheri_base_get(parent) &&		\
+	 cheri_top_get(ptr) <= cheri_top_get(parent) &&			\
+	 (cheri_perms_get(ptr) & cheri_perms_get(parent)) == cheri_perms_get(ptr))
+
+#define	cheri_is_null_derived(x)					\
+	cheri_is_equal_exact((uintptr_t)cheri_address_get(x), x)
+
+/* Create an untagged capability from an integer */
+#define cheri_fromint(x)	cheri_offset_set(NULL, x)
+
+/* Increment @p dst to have the address of @p src */
+#define cheri_address_copy(dst, src)					\
+	(cheri_address_set(dst, cheri_address_get(src)))
+
+#define	cheri_offset_inc(x, y)	__builtin_cheri_offset_increment((x), (y))
+
+/* Get the top of a capability (i.e. one byte past the last accessible one) */
+#define	cheri_top_get(cap)	__extension__({			\
+	__typeof__(cap) c = (cap);				\
+	(cheri_base_get(c) + cheri_length_get(c));		\
+})
+
+/* Check if the address is between cap.base and cap.top, i.e. in bounds */
+static inline bool
+cheri_is_address_inbounds(const void *cap, ptraddr_t addr)
+{
+	return (addr >= cheri_base_get(cap) && addr < cheri_top_get(cap));
+}
+
+#ifdef _KERNEL
+/*
+ * Check if the capability is valid, unsealed, has the given permissions and
+ * grants access to length bytes at address base.
+ */
+static inline bool
+cheri_can_access(const void *cap, ptraddr_t perms, ptraddr_t base,
+    size_t length)
+{
+	return (cheri_tag_get(cap) && !cheri_is_sealed(cap) &&
+	    (cheri_perms_get(cap) & perms) == perms &&
+	    base >= cheri_base_get(cap) && base + length <= cheri_top_get(cap));
+}
+#endif
+
+static inline size_t
+cheri_bytes_remaining(const void *cap)
+{
+	if (cheri_offset_get(cap) >= cheri_length_get(cap))
+		return 0;
+	return cheri_length_get(cap) - cheri_offset_get(cap);
+}
+
+#define	cheri_stack_get()	__builtin_cheri_stack_get()
+
+#endif	/* __has_feature(capabilities) */
+
+#ifdef _KERNEL
+#ifdef __CHERI_PURE_CAPABILITY__
+#define	cheri_kern_tag_get(x)		cheri_tag_get(x)
+#define	cheri_kern_bounds_set(x, y)	cheri_bounds_set(x, y)
+#define	cheri_kern_bounds_set_exact(x, y)	cheri_bounds_set_exact(x, y)
+#define	cheri_kern_address_set(x, y)	cheri_address_set(x, y)
+#define	cheri_kern_address_get(x)	cheri_address_get(x)
+#define	cheri_kern_perms_and(x, y)	cheri_perms_and(x, y)
+#else
+#define	cheri_kern_tag_get(x)		1
+#define	cheri_kern_bounds_set(x, y)	(x)
+#define	cheri_kern_bounds_set_exact(x, y)	(x)
+#define	cheri_kern_address_set(x, y)	((__typeof__(x))(y))
+#define	cheri_kern_address_get(x)	((uintptr_t)(x))
+#define	cheri_kern_perms_and(x, y)	(x)
+#endif	/* __CHERI_PURE_CAPABILITY__ */
+#endif	/* _KERNEL */
+
+/*
+ * The cheri_{get,set,clear}_low_pointer_bits() functions work both with and
+ * without CHERI support so can be used unconditionally to fix
+ * -Wcheri-bitwise-operations warnings.
+ *
+ * XXXAR: Should kept in sync with the version from clang's cheri.h.
+ */
+
+static inline __result_use_check size_t
+__cheri_get_low_ptr_bits(uintptr_t ptr, size_t mask) {
+	/*
+	 * Note: we continue to use bitwise and on the uintcap value and silence
+	 * the warning instead of using __builtin_cheri_offset_get() in case
+	 * we decide to use a virtual-address instead offset interpretation of
+	 * capabilities in the future.
+	 * We mustn't return a LHS-derived capability here so we need to
+	 * explicitly cast the result to a non-capability integer
+	 */
+	return (size_t)(ptr & mask);
+}
+
+static inline __result_use_check uintptr_t
+__cheri_set_low_ptr_bits(uintptr_t ptr, size_t bits) {
+	/*
+	 * We want to return a LHS-derived capability here so using the default
+	 * uintptr_t semantics is fine.
+	 */
+	return ptr | bits;
+}
+
+static inline __result_use_check uintptr_t
+__cheri_clear_low_ptr_bits(uintptr_t ptr, size_t bits_mask) {
+	/*
+	 * We want to return a LHS-derived capability here so using the default
+	 * uintptr_t semantics is fine.
+	 */
+	return ptr & (~bits_mask);
+}
+
+/* Turn on the checking by default for now (until we have fixed everything)*/
+#define __check_low_ptr_bits_assignment
+#if defined(_KERNEL) || !defined(assert) /* Don't pull in assert.h when building the kernel */
+#define _cheri_bits_assert(e) (void)0
+#endif
+#ifdef __check_low_ptr_bits_assignment
+#ifndef _cheri_bits_assert
+#define _cheri_bits_assert(e) assert(e)
+#endif
+#define __runtime_assert_sensible_low_bits(bits)                               \
+  __extension__({                                                              \
+    _cheri_bits_assert((bits) < 32 && "Should only use the low 5 pointer bits"); \
+    bits;                                                                      \
+  })
+#else
+#define __runtime_assert_sensible_low_bits(bits) bits
+#endif
+#define __static_assert_sensible_low_bits(bits)                                \
+  __extension__({                                                              \
+    _Static_assert((bits) < 32, "Should only use the low 5 pointer bits");     \
+    bits;                                                                      \
+  })
+
+/*
+ * Get the low bits defined in @p mask from the capability/pointer @p ptr.
+ * @p mask must be a compile-time constant less than 31.
+ * TODO: should we allow non-constant masks?
+ *
+ * @param ptr the uintptr_t that may have low bits sets
+ * @param mask the mask for the low pointer bits to retrieve
+ * @return a size_t containing the the low bits from @p ptr
+ *
+ * Rationale: this function is needed because extracting the low bits using a
+ * bitwise-and operation returns a LHS-derived capability with the offset
+ * field set to LHS.offset & mask. This is almost certainly not what the user
+ * wanted since it will always compare not equal to any integer constant.
+ * For example lots of mutex code uses something like `if ((x & 1) == 1)` to
+ * detect if the lock is currently contented. This comparison always returns
+ * false under CHERI the LHS of the == is a valid capability with offset 3 and
+ * the RHS is an untagged intptr_t with offset 3.
+ * See https://github.com/CTSRD-CHERI/clang/issues/189
+ */
+#define cheri_get_low_ptr_bits(ptr, mask)                                      \
+  __cheri_get_low_ptr_bits((uintptr_t)(ptr), __static_assert_sensible_low_bits(mask))
+
+/*
+ * Set low bits in a uintptr_t
+ *
+ * @param ptr the uintptr_t that may have low bits sets
+ * @param bits the value to bitwise-or with @p ptr.
+ * @return a uintptr_t that has the low bits defined in @p mask set to @p bits
+ *
+ * @note this function is not strictly required since a plain bitwise or will
+ * generally give the behaviour that is expected from other platforms but.
+ * However, we can't really make the warning "-Wcheri-bitwise-operations"
+ * trigger based on of the right hand side expression since it may not be a
+ * compile-time constant.
+ */
+#define cheri_set_low_ptr_bits(ptr, bits)                                      \
+  __cheri_set_low_ptr_bits((uintptr_t)(ptr), __runtime_assert_sensible_low_bits(bits))
+
+/*
+ * Clear the bits in @p mask from the capability/pointer @p ptr.
+ *
+ * @param ptr the uintptr_t that may have low bits sets
+ * @param mask this is the mask for the low pointer bits, not the mask for
+ * the bits that should remain set.
+ * @return a uintptr_t that has the low bits defined in @p mask set to zeroes
+ *
+ * @note this function is not strictly required since a plain bitwise or will
+ * generally give the behaviour that is expected from other platforms but.
+ * However, we can't really make the warning "-Wcheri-bitwise-operations"
+ * trigger based on of the right hand side expression since it may not be a
+ * compile-time constant.
+ *
+ */
+#define cheri_clear_low_ptr_bits(ptr, mask)                                    \
+  __cheri_clear_low_ptr_bits((uintptr_t)(ptr), __runtime_assert_sensible_low_bits(mask))
+
+#if __has_feature(capabilities)
+#define	CHERI_REPRESENTABLE_LENGTH(len) \
+	__builtin_cheri_round_representable_length(len)
+#define	CHERI_REPRESENTABLE_ALIGNMENT_MASK(len) \
+	__builtin_cheri_representable_alignment_mask(len)
+
+#define	CHERI_ALIGN_SHIFT(l)	\
+	__builtin_ctzll(CHERI_REPRESENTABLE_ALIGNMENT_MASK(l))
+
+#else /* !__has_feature(capabilities) */
+#define	CHERI_REPRESENTABLE_LENGTH(len) (len)
+#define	CHERI_REPRESENTABLE_ALIGNMENT_MASK(len) UINT64_MAX
+#endif /* !__has_feature(capabilities) */
+
+/* Provide macros to make it easier to work with the raw CRAM/CRRL results: */
+#define	CHERI_REPRESENTABLE_ALIGNMENT(len) \
+	(~CHERI_REPRESENTABLE_ALIGNMENT_MASK(len) + 1)
+#define	CHERI_REPRESENTABLE_ALIGN_DOWN(base, len) \
+	((base) & CHERI_REPRESENTABLE_ALIGNMENT_MASK(len))
+
+#if __has_feature(capabilities)
+#define	CHERI_REPRESENTABLE_ALIGN_UP(base, len) \
+	__align_up((base), CHERI_REPRESENTABLE_ALIGNMENT(len))
+#else
+#define	CHERI_REPRESENTABLE_ALIGN_UP(base, len) (base)
+#endif
+
+#define	CHERI_ALIGN_MASK(l)		~(CHERI_REPRESENTABLE_ALIGNMENT_MASK(l))
+
+#if __has_feature(capabilities)
+#include <machine/cheric.h>
+#endif
+
+#ifndef _KERNEL
+ssize_t	strfcap(char * __restrict buf, size_t maxsize,
+    const char * __restrict format, uintptr_t cap);
+#endif
+
+#endif /* _SYS_CHERIC_H_ */
