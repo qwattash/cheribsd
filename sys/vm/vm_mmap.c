30eda3e5abffc551fc4fa9b964d4e6c92ac631e8
Brooks Davis <brooks@FreeBSD.org>
2025-12-19 11:05:25 +0000

    mmap: Add CHERI PROT_ flags
    PROT_CAP, when combined with PROT_READ and/or PROT_WRITE causes mappings
    to support loading or storing CHERI capabilities (subject to a number
    of restrictions based on the mapping request).
    
    If either of PROT_CAP or PROT_NO_CAP are set, then the value of
    the PROT_CAP flag bit defines the page protections and capability
    permissions for a given mapping.
    
    In the upcoming implementation, PROT_CAP maps to VM_PROT_READ_CAP and
    VM_PROT_WRITE_CAP depending on the values of PROT_READ and PROT_WRITE.
    PROT_NO_CAP maps to VM_PROT_NO_IMPLY_CAP to avoid add _CAP permissions
    when explicitly requested not to or to allow supeset tests to succeed on
    max_protection when reducing permissions.
    
    Effort:		CHERI upstreaming
    Sponsored by:	Innovate UK, DARPA, AFRL

--- sys/vm/vm_mmap.c
+++ sys/vm/vm_mmap.c
@@ -167,11 +167,11 @@ kern_mmap_maxprot(struct proc *p, int prot)
 
 	if ((p->p_flag2 & P2_PROTMAX_DISABLE) != 0 ||
 	    (p->p_fctl0 & NT_FREEBSD_FCTL_PROTMAX_DISABLE) != 0)
-		return (_PROT_ALL);
+		return (PROT_READ | PROT_WRITE | PROT_EXEC);
 	if (((p->p_flag2 & P2_PROTMAX_ENABLE) != 0 || imply_prot_max) &&
 	    prot != PROT_NONE)
 		 return (prot);
-	return (_PROT_ALL);
+	return (PROT_READ | PROT_WRITE | PROT_EXEC);
 }
 
 int
