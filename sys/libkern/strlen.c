5008d24a4ac6e2b28c84feecfacd9060b9b316b4
Brooks Davis <brooks@FreeBSD.org>
2025-12-19 15:03:51 +0000

    libkern/strlen.c: make it work on CHERI
    On CHERI we can't count on any particularly alignment of the front or
    end of the capability and thus need to avoid reads which might land
    outside the bounds in whole or in part until we make a final read
    entierly outside the bounds in the case that the string is unterminated.
    
    This implementation could be better in that it checks how much is left
    of the capability on each loop rather than only doing so if the end is
    under-aligned.
    
    The non-CHERI case is unchanged.
    
    Effort:		CHERI upstreaming
    Sponsored by:	Innovate UK, DARPA, AFRL

--- sys/libkern/strlen.c
+++ sys/libkern/strlen.c
@@ -58,8 +58,6 @@ static const unsigned long mask80 = 0x8080808080808080;
 #error Unsupported word size
 #endif
 
-#define	LONGPTR_MASK (sizeof(long) - 1)
-
 /*
  * Helper macro to return string length if we caught the zero
  * byte.
@@ -76,6 +74,7 @@ size_t
 	const char *p;
 	const unsigned long *lp;
 	long va, vb;
+	bool byte_check;
 
 	/*
 	 * Before trying the hard (unaligned byte-by-byte access) way
@@ -86,19 +85,35 @@ size_t
 	 * p and (p & ~LONGPTR_MASK) must be equally accessible since
 	 * they always fall in the same memory page, as long as page
 	 * boundaries is integral multiple of word size.
+	 *
+	 * This is not true for CHERI, so we skip directly to byte
+	 * access if not word-aligned.
 	 */
-	lp = (const unsigned long *)((uintptr_t)str & ~LONGPTR_MASK);
+	lp = (const unsigned long *)rounddown2(str, sizeof(long));
+#ifdef __CHERI__
+	byte_check = ((const char *)lp < str);
+	if (byte_check)
+		lp++;
+#else
 	va = (*lp - mask01);
 	vb = ((~*lp) & mask80);
 	lp++;
-	if (va & vb)
+	byte_check = (bool)(va & vb);
+#endif
+	if (byte_check)
 		/* Check if we have \0 in the first part */
 		for (p = str; p < (const char *)lp; p++)
 			if (*p == '\0')
 				return (p - str);
 
 	/* Scan the rest of the string using word sized operation */
-	for (; ; lp++) {
+#ifdef __CHERI__
+	/* XXX: we could skip the test if the end if well aligned. */
+	for (; cheri_length_get(lp) - cheri_offset_get(lp) >= sizeof(long); lp++)
+#else
+	for (; ; lp++)
+#endif
+	{
 		va = (*lp - mask01);
 		vb = ((~*lp) & mask80);
 		if (va & vb) {
@@ -115,6 +130,18 @@ size_t
 #endif
 		}
 	}
+
+#ifdef __CHERI__
+	/*
+	 * If the end of the capability is misaligned, we need to scan
+	 * byte-by-byte at the end.
+	 */
+	if ((ptraddr_t)lp != cheri_top_get(lp)) {
+		for (p = (const char *)lp; ; p++)
+			if (*p == '\0')
+				return (p - str);
+	}
+#endif
 
 	/* NOTREACHED */
 	return (0);
