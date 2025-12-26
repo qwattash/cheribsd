c01fe5c0f9b2d6408211b955b51d71f232c7fac8
Brooks Davis <brooks@FreeBSD.org>
2025-12-26 20:40:35 +0000

    CHERI: silence a spurious alignment warning
    When copying a struct sockaddr_in(6) that follows an mtag, cast to let
    the compiler know it doesn't contain pointers.  Otherwise the compiler
    warns that it's copying to an underaligned destintation.
    
    Effort:		CHERI upstreaming
    Sponsored by:	Innovate UK

--- sys/netinet6/ip6_output.c
+++ sys/netinet6/ip6_output.c
@@ -1072,7 +1072,8 @@ nonh6lookup:
 			dst = (struct sockaddr_in6 *)&ro->ro_dst;
 		else
 			dst = &sin6;
-		bcopy((fwd_tag+1), &dst_sa, sizeof(struct sockaddr_in6));
+		bcopy((struct sockaddr_in6 *)(fwd_tag+1), &dst_sa,
+		    sizeof(struct sockaddr_in6));
 		m->m_flags |= M_SKIP_FIREWALL;
 		m->m_flags &= ~M_IP6_NEXTHOP;
 		m_tag_delete(m, fwd_tag);
