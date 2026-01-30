c0e01dac61f182bc118932a8d5df659e31cccf98
Brooks Davis <brooks@FreeBSD.org>
2026-01-30 15:04:22 +0000

    cheri: declare fueptr and suptr
    These should replace fueword and suword when manipulating single
    pointeres in memory.  On CHERI systems they are implemented using
    capability aware instructions and otherwise they are defined to fueword
    and suword.
    
    Effort:		CHERI upstreaming
    Sponsored by:	Innovate UK

--- sys/sys/systm.h
+++ sys/sys/systm.h
@@ -361,11 +361,21 @@ int64_t	fuword64(volatile const void *base);
 int __result_use_check fueword(volatile const void *base, long *val);
 int __result_use_check fueword32(volatile const void *base, int32_t *val);
 int __result_use_check fueword64(volatile const void *base, int64_t *val);
+#ifdef __CHERI__
+int __result_use_check fueptr(volatile const void *base, intptr_t *val);
+#else
+#define	fueptr(base, val)	fueword((base), (long *)(val))
+#endif
 __nodiscard int subyte(volatile void *base, int byte);
 __nodiscard int suword(volatile void *base, long word);
 __nodiscard int suword16(volatile void *base, int word);
 __nodiscard int suword32(volatile void *base, int32_t word);
 __nodiscard int suword64(volatile void *base, int64_t word);
+#ifdef __CHERI__
+__nodiscard int suptr(volatile void *base, intptr_t ptr);
+#else
+#define	suptr(base, val)	suword((base), (val))
+#endif
 uint32_t casuword32(volatile uint32_t *base, uint32_t oldval, uint32_t newval);
 u_long	casuword(volatile u_long *p, u_long oldval, u_long newval);
 int	casueword32(volatile uint32_t *base, uint32_t oldval, uint32_t *oldvalp,
