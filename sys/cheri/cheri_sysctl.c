54776b5904c843643ad055dbe3c3b4e6ebffb980
Robert N. M. Watson <robert.watson@cl.cam.ac.uk>
2026-02-16 12:03:48 +0000

    CHERI: add security.cheri sysctl branch
    Effort:		CHERI upstreaming
    Sponsored by:	Innovate UK

--- sys/cheri/cheri_sysctl.c
+++ sys/cheri/cheri_sysctl.c
@@ -1,19 +1,18 @@
 /*-
- * Copyright (c) 2011-2017 Robert N. M. Watson
- * Copyright (c) 2015 SRI International
+ * Copyright (c) 2011-2016 Robert N. M. Watson
  * All rights reserved.
- * 
+ *
  * This software was developed by SRI International and the University of
  * Cambridge Computer Laboratory under DARPA/AFRL contract (FA8750-10-C-0237)
  * ("CTSRD"), as part of the DARPA CRASH research programme.
  *
- * Redistribution and use in source and binary forms, with or without 
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 1. Redistributions of source code must retain the above copyright  
- *    notice, this list of conditions and the following disclaimer. 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the  
+ *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
@@ -21,30 +20,24 @@
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS   
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
 
-#ifndef _CHERI_CHERI_H_
-#define _CHERI_CHERI_H_
+#include "opt_ddb.h"
 
-#ifdef _KERNEL
+#include <sys/param.h>
+#include <sys/sysctl.h>
+#include <sys/systm.h>
 
-/* Root kernel capability */
-extern void *kernel_root_cap;
+#include <cheri/cheri.h>
 
-/*
- * Global sysctl definitions.
- */
-SYSCTL_DECL(_security_cheri);
-SYSCTL_DECL(_security_cheri_stats);
+SYSCTL_NODE(_security, OID_AUTO, cheri, CTLFLAG_RD, 0,
+    "CHERI settings and statistics");
 
-#endif /* _KERNEL */
-
-#include <machine/cheri.h>
-
-#endif /* _CHERI_CHERI_H_ */
+SYSCTL_NODE(_security_cheri, OID_AUTO, stats, CTLFLAG_RD, 0,
+    "CHERI statistics");
