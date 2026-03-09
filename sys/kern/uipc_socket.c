c8d90ddfd491b87ef2789dd92d3d958ed58f30fb
Brooks Davis <brooks@FreeBSD.org>
2026-03-09 17:11:03 +0000

    socket options: use sooptcopyinptr for MAC labels
    Move the copy into an sooptcopyinmac function and implement freebsd32
    compat in the process.
    
    Effort:		CHERI upstreaming
    Sponsored by:	Innovate UK

--- sys/kern/uipc_socket.c
+++ sys/kern/uipc_socket.c
@@ -110,6 +110,7 @@
 
 #include <sys/param.h>
 #include <sys/systm.h>
+#include <sys/abi_compat.h>
 #include <sys/capsicum.h>
 #include <sys/fcntl.h>
 #include <sys/limits.h>
@@ -131,6 +132,7 @@
 #include <sys/proc.h>
 #include <sys/protosw.h>
 #include <sys/sbuf.h>
+#include <sys/sysent.h>
 #include <sys/socket.h>
 #include <sys/socketvar.h>
 #include <sys/resourcevar.h>
@@ -3819,6 +3821,30 @@ sooptcopyinptr(struct sockopt *sopt, void *buf, size_t len, size_t minlen)
 }
 #endif
 
+#ifdef MAC
+static int
+sooptcopyinmac(struct sockopt *sopt, struct mac *mac)
+{
+	int error = 0;
+
+	memset(mac, 0, sizeof(*mac));
+#ifdef COMPAT_FREEBSD32
+	if (SV_CURPROC_FLAG(SV_ILP32)) {
+		struct mac32 m32;
+
+		error = sooptcopyin(sopt, &m32, sizeof(m32), sizeof(m32));
+		if (error)
+			return (error);
+		CP(m32, *mac, m_buflen);
+		PTRIN_CP(m32, *mac, m_string);
+	} else
+#endif
+		error = sooptcopyinptr(sopt, mac, sizeof(*mac), sizeof(*mac));
+
+	return (error);
+}
+#endif /* MAC */
+
 /*
  * Kernel version of setsockopt(2).
  *
@@ -3967,8 +3993,7 @@ sosetopt(struct socket *so, struct sockopt *sopt)
 
 		case SO_LABEL:
 #ifdef MAC
-			error = sooptcopyin(sopt, &extmac, sizeof extmac,
-			    sizeof extmac);
+			error = sooptcopyinmac(sopt, &extmac);
 			if (error)
 				goto bad;
 			error = mac_setsockopt_label(sopt->sopt_td->td_ucred,
@@ -4234,8 +4259,7 @@ integer:
 
 		case SO_LABEL:
 #ifdef MAC
-			error = sooptcopyin(sopt, &extmac, sizeof(extmac),
-			    sizeof(extmac));
+			error = sooptcopyinmac(sopt, &extmac);
 			if (error)
 				goto bad;
 			error = mac_getsockopt_label(sopt->sopt_td->td_ucred,
@@ -4250,8 +4274,7 @@ integer:
 
 		case SO_PEERLABEL:
 #ifdef MAC
-			error = sooptcopyin(sopt, &extmac, sizeof(extmac),
-			    sizeof(extmac));
+			error = sooptcopyinmac(sopt, &extmac);
 			if (error)
 				goto bad;
 			error = mac_getsockopt_peerlabel(
