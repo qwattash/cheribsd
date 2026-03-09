c1c27c147494302b2446477a1436893c66c9a676
Brooks Davis <brooks@FreeBSD.org>
2026-03-09 13:14:53 +0000

    CHERI: check bounds on sglist before use
    Check that the capability covers the right region since DMA (etc.)
    don't currently.  Callers need to verify permissions as we no longer
    know that the right ones at this point.
    
    Effort:		CHERI upstreaming
    Sponsored by:	DARPA, AFRL, Innovate UK

--- sys/kern/subr_sglist.c
+++ sys/kern/subr_sglist.c
@@ -114,6 +114,16 @@ _sglist_append_buf(struct sglist *sg, void *buf, size_t len, pmap_t pmap,
 	size_t seglen;
 	int error;
 
+#ifdef __CHERI__
+	/*
+	 * The memory at *buf will be accessed via physical addressing
+	 * (typically DMA).  The caller should verify the capability
+	 * permissions.
+	 */
+	if (!cheri_can_access(buf, 0, len))
+		return (EPROT);
+#endif
+
 	if (donep)
 		*donep = 0;
 	if (len == 0)
