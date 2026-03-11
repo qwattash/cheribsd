CHERI-Alliance/freebsd-src

kenv(2): add kern_ for syscall to wrap
Effort:		CHERI upstreaming
Sponsored by:	Innovate UK

sys/kern/kern_environment.c
@@ -49,6 +49,7 @@
 #include <sys/priv.h>
 #include <sys/proc.h>
 #include <sys/queue.h>
+#include <sys/syscallsubr.h>
 #include <sys/sysent.h>
 #include <sys/sysproto.h>
 
@@ -158,6 +159,13 @@ kenv_dump(struct thread *td, char **envp, int what, char *value, int len)
 int
 sys_kenv(struct thread *td, struct kenv_args *uap)
 {
+	return (kern_kenv(td, uap->what, uap->name, uap->value, uap->len));
+}
+
+int
+kern_kenv(struct thread *td, int what, const char *namep, char *u_val,
+    int u_len)
+{
 	char *name, *value;
 	size_t len;
 	int error;
@@ -166,14 +174,14 @@ sys_kenv(struct thread *td, struct kenv_args *uap)
 
 	error = 0;
 
-	switch (uap->what) {
+	switch (what) {
 	case KENV_DUMP:
 #ifdef MAC
 		error = mac_kenv_check_dump(td->td_ucred);
 		if (error)
 			return (error);
 #endif
-		return (kenv_dump(td, kenvp, uap->what, uap->value, uap->len));
+		return (kenv_dump(td, kenvp, what, u_val, u_len));
 	case KENV_DUMP_LOADER:
 	case KENV_DUMP_STATIC:
 #ifdef MAC
@@ -183,8 +191,8 @@ sys_kenv(struct thread *td, struct kenv_args *uap)
 #endif
 #ifdef PRESERVE_EARLY_KENV
 		return (kenv_dump(td,
-		    uap->what == KENV_DUMP_LOADER ? (char **)md_envp :
-		    (char **)kern_envp, uap->what, uap->value, uap->len));
+		    what == KENV_DUMP_LOADER ? (char **)md_envp :
+		    (char **)kern_envp, what, u_val, u_len));
 #else
 		return (ENOENT);
 #endif
@@ -203,11 +211,11 @@ sys_kenv(struct thread *td, struct kenv_args *uap)
 
 	name = malloc(KENV_MNAMELEN + 1, M_TEMP, M_WAITOK);
 
-	error = copyinstr(uap->name, name, KENV_MNAMELEN + 1, NULL);
+	error = copyinstr(name, name, KENV_MNAMELEN + 1, NULL);
 	if (error)
 		goto done;
 
-	switch (uap->what) {
+	switch (what) {
 	case KENV_GET:
 #ifdef MAC
 		error = mac_kenv_check_get(td->td_ucred, name);
@@ -220,16 +228,16 @@ sys_kenv(struct thread *td, struct kenv_args *uap)
 			goto done;
 		}
 		len = strlen(value) + 1;
-		if (len > uap->len)
-			len = uap->len;
-		error = copyout(value, uap->value, len);
+		if (len > u_len)
+			len = u_len;
+		error = copyout(value, u_val, len);
 		freeenv(value);
 		if (error)
 			goto done;
 		td->td_retval[0] = len;
 		break;
 	case KENV_SET:
-		len = uap->len;
+		len = u_len;
 		if (len < 1) {
 			error = EINVAL;
 			goto done;
@@ -237,7 +245,7 @@ sys_kenv(struct thread *td, struct kenv_args *uap)
 		if (len > kenv_mvallen + 1)
 			len = kenv_mvallen + 1;
 		value = malloc(len, M_TEMP, M_WAITOK);
-		error = copyinstr(uap->value, value, len, NULL);
+		error = copyinstr(u_val, value, len, NULL);
 		if (error) {
 			free(value, M_TEMP);
 			goto done;

qwattash/cheribsd

sys/kern/kern_environment.c
@@ -49,6 +49,7 @@
 #include <sys/priv.h>
 #include <sys/proc.h>
 #include <sys/queue.h>
+#include <sys/syscallsubr.h>
 #include <sys/sysent.h>
 #include <sys/sysproto.h>
 
@@ -158,6 +159,13 @@ kenv_dump(struct thread *td, char **envp, int what, char *value, int len)
 int
 sys_kenv(struct thread *td, struct kenv_args *uap)
 {
+	return (kern_kenv(td, uap->what, uap->name, uap->value, uap->len));
+}
+
+int
+kern_kenv(struct thread *td, int what, const char *namep, char *u_val,
+    int u_len)
+{
 	char *name, *value;
 	size_t len;
 	int error;
@@ -166,14 +174,14 @@ sys_kenv(struct thread *td, struct kenv_args *uap)
 
 	error = 0;
 
-	switch (uap->what) {
+	switch (what) {
 	case KENV_DUMP:
 #ifdef MAC
 		error = mac_kenv_check_dump(td->td_ucred);
 		if (error)
 			return (error);
 #endif
-		return (kenv_dump(td, kenvp, uap->what, uap->value, uap->len));
+		return (kenv_dump(td, kenvp, what, u_val, u_len));
 	case KENV_DUMP_LOADER:
 	case KENV_DUMP_STATIC:
 #ifdef MAC
@@ -183,8 +191,8 @@ sys_kenv(struct thread *td, struct kenv_args *uap)
 #endif
 #ifdef PRESERVE_EARLY_KENV
 		return (kenv_dump(td,
-		    uap->what == KENV_DUMP_LOADER ? (char **)md_envp :
-		    (char **)kern_envp, uap->what, uap->value, uap->len));
+		    what == KENV_DUMP_LOADER ? (char **)md_envp :
+		    (char **)kern_envp, what, u_val, u_len));
 #else
 		return (ENOENT);
 #endif
@@ -203,11 +211,11 @@ sys_kenv(struct thread *td, struct kenv_args *uap)
 
 	name = malloc(KENV_MNAMELEN + 1, M_TEMP, M_WAITOK);
 
-	error = copyinstr(uap->name, name, KENV_MNAMELEN + 1, NULL);
+	error = copyinstr(namep, name, KENV_MNAMELEN + 1, NULL);
 	if (error)
 		goto done;
 
-	switch (uap->what) {
+	switch (what) {
 	case KENV_GET:
 #ifdef MAC
 		error = mac_kenv_check_get(td->td_ucred, name);
@@ -220,16 +228,16 @@ sys_kenv(struct thread *td, struct kenv_args *uap)
 			goto done;
 		}
 		len = strlen(value) + 1;
-		if (len > uap->len)
-			len = uap->len;
-		error = copyout(value, uap->value, len);
+		if (len > u_len)
+			len = u_len;
+		error = copyout(value, u_val, len);
 		freeenv(value);
 		if (error)
 			goto done;
 		td->td_retval[0] = len;
 		break;
 	case KENV_SET:
-		len = uap->len;
+		len = u_len;
 		if (len < 1) {
 			error = EINVAL;
 			goto done;
@@ -237,7 +245,7 @@ sys_kenv(struct thread *td, struct kenv_args *uap)
 		if (len > kenv_mvallen + 1)
 			len = kenv_mvallen + 1;
 		value = malloc(len, M_TEMP, M_WAITOK);
-		error = copyinstr(uap->value, value, len, NULL);
+		error = copyinstr(u_val, value, len, NULL);
 		if (error) {
 			free(value, M_TEMP);
 			goto done;