The-Capable-Hub/freebsd-src

aarch64: add MD CHERI support files
Of particular interest, cheri_machdep.c holds and initialized the
global variables created at startup from the omnipotent reset
capability.

Co-authored-by:	Andrew Turner <at718@cam.ac.uk>
Co-authored-by:	Brett Gutstein <brett.gutstein@cst.cam.ac.uk>
Co-authored-by:	Jessica Clarke <jrtc27@jrtc27.com>
Co-authored-by: John Baldwin <jhb@FreeBSD.org>

sys/arm64/cheri/cheri_machdep.c
@@ -1,0 +1,135 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2020 John Baldwin
+ *
+ * This software was developed by SRI International and the University of
+ * Cambridge Computer Laboratory (Department of Computer Science and
+ * Technology) under DARPA contract HR0011-18-C-0016 ("ECATS"), as part of the
+ * DARPA SSITH research programme.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/param.h>
+#include <sys/kernel.h>
+#include <sys/devmap.h>
+#include <sys/proc.h>
+
+#include <cheri/cheri.h>
+#include <cheri/cheric.h>
+
+#include <arm64/vmm/hyp.h>
+#include <machine/frame.h>
+#include <machine/pte.h>
+#include <machine/vmparam.h>
+
+void * __capability sentry_unsealcap;
+void * __capability smccc_ddc_el0;
+void * __capability vmm_gva_root_cap = (void * __capability)(intptr_t)-1;
+void * __capability vmm_gpa_root_cap = (void * __capability)(intptr_t)-1;
+void *kernel_root_cap = (void *)(intptr_t)-1;
+void *vmm_el2_root_cap = (void *)(intptr_t)-1;
+
+void __nosanitizecoverage
+cheri_init_capabilities(void * __capability kroot)
+{
+	void * __capability ctemp;
+
+#ifdef notyet
+	ctemp = cheri_address_set(kroot, CHERI_SEALCAP_KERNEL_BASE);
+	ctemp = cheri_bounds_set(ctemp, CHERI_SEALCAP_KERNEL_LENGTH);
+	ctemp = cheri_perms_and(ctemp, CHERI_SEALCAP_KERNEL_PERMS);
+	kernel_root_sealcap = ctemp;
+#endif
+
+	ctemp = cheri_address_set(kroot, CHERI_CAP_USER_DATA_BASE);
+	ctemp = cheri_bounds_set(ctemp, CHERI_CAP_USER_DATA_LENGTH);
+	ctemp = cheri_perms_and(ctemp, CHERI_CAP_USER_DATA_PERMS |
+	    CHERI_CAP_USER_CODE_PERMS | CHERI_PERM_SW_VMEM);
+	userspace_root_cap = ctemp;
+
+#ifdef notyet
+	ctemp = cheri_address_set(kroot, CHERI_SEALCAP_USERSPACE_BASE);
+	ctemp = cheri_bounds_set(ctemp, CHERI_SEALCAP_USERSPACE_LENGTH);
+	ctemp = cheri_perms_and(ctemp, CHERI_SEALCAP_USERSPACE_PERMS);
+	userspace_root_sealcap = ctemp;
+#endif
+
+#ifdef notyet
+	ctemp = cheri_address_set(kroot, CHERI_COMPARTMENT_ID_USERSPACE_BASE);
+	ctemp = cheri_bounds_set(ctemp, CHERI_COMPARTMENT_ID_USERSPACE_LENGTH);
+	ctemp = cheri_perms_and(ctemp, CHERI_COMPARTMENT_ID_USERSPACE_PERMS);
+	userspace_root_cidcap = (uintptr_t)ctemp;
+#endif
+
+	ctemp = cheri_address_set(kroot, CHERI_OTYPE_SENTRY);
+	ctemp = cheri_bounds_set(ctemp, 1);
+	ctemp = cheri_perms_and(ctemp, CHERI_PERM_GLOBAL | CHERI_PERM_UNSEAL);
+	sentry_unsealcap = ctemp;
+
+	smccc_ddc_el0 = kroot;
+
+#ifdef notyet
+	swap_restore_cap = kroot;
+#endif
+
+#ifdef notyet
+	vmm_gva_root_cap = kroot;
+
+	vmm_gpa_root_cap = cheri_address_set(kroot, HYP_GPA_MIN_ADDRESS);
+	vmm_gpa_root_cap = cheri_bounds_set(vmm_gpa_root_cap,
+	    HYP_GPA_MAX_ADDRESS - HYP_GPA_MIN_ADDRESS);
+#endif
+
+	ctemp = cheri_address_set(kroot, VM_MAX_KERNEL_ADDRESS -
+	    PMAP_MAPDEV_EARLY_SIZE);
+	ctemp = cheri_bounds_set_exact(ctemp, PMAP_MAPDEV_EARLY_SIZE);
+	ctemp = cheri_perms_and(ctemp, CHERI_PERMS_KERNEL_DATA);
+	devmap_init_capability(ctemp);
+
+	kernel_root_cap = cheri_perms_and(kroot,
+	    ~(CHERI_PERM_SEAL | CHERI_PERM_UNSEAL));
+
+	vmm_el2_root_cap = cheri_address_set(kroot, HYP_VM_MIN_ADDRESS);
+	vmm_el2_root_cap = cheri_bounds_set(vmm_el2_root_cap,
+	    HYP_VM_MAX_ADDRESS - HYP_VM_MIN_ADDRESS);
+}
+
+void
+hybridabi_thread_setregs(struct thread *td, unsigned long entry_addr)
+{
+	struct trapframe *tf;
+
+	tf = td->td_frame;
+
+	/* Set DDC to full user privilege. */
+	tf->tf_ddc = (uintptr_t)cheri_capability_build_user_rwx(
+	    CHERI_CAP_USER_DATA_PERMS | CHERI_PERM_SW_VMEM,
+	    CHERI_CAP_USER_DATA_BASE, CHERI_CAP_USER_DATA_LENGTH,
+	    CHERI_CAP_USER_DATA_OFFSET);
+
+	/* Use 'entry_addr' as offset of PCC. */
+	trapframe_set_elr(tf, (uintptr_t)cheri_capability_build_user_code(
+	    td, CHERI_CAP_USER_CODE_PERMS, CHERI_CAP_USER_CODE_BASE,
+	    CHERI_CAP_USER_CODE_LENGTH, entry_addr));
+}

qwattash/cheribsd

sys/arm64/cheri/cheri_machdep.c
@@ -1,0 +1,135 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2020 John Baldwin
+ *
+ * This software was developed by SRI International and the University of
+ * Cambridge Computer Laboratory (Department of Computer Science and
+ * Technology) under DARPA contract HR0011-18-C-0016 ("ECATS"), as part of the
+ * DARPA SSITH research programme.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/param.h>
+#include <sys/kernel.h>
+#include <sys/devmap.h>
+#include <sys/proc.h>
+
+#include <cheri/cheri.h>
+#include <cheri/cheric.h>
+
+#include <arm64/vmm/hyp.h>
+#include <machine/frame.h>
+#include <machine/pte.h>
+#include <machine/vmparam.h>
+
+void * __capability sentry_unsealcap;
+void * __capability smccc_ddc_el0;
+void * __capability vmm_gva_root_cap = (void * __capability)(intptr_t)-1;
+void * __capability vmm_gpa_root_cap = (void * __capability)(intptr_t)-1;
+void *kernel_root_cap = (void *)(intptr_t)-1;
+void *vmm_el2_root_cap = (void *)(intptr_t)-1;
+
+void __nosanitizecoverage
+cheri_init_capabilities(void * __capability kroot)
+{
+	void * __capability ctemp;
+
+#ifdef notyet
+	ctemp = cheri_address_set(kroot, CHERI_SEALCAP_KERNEL_BASE);
+	ctemp = cheri_bounds_set(ctemp, CHERI_SEALCAP_KERNEL_LENGTH);
+	ctemp = cheri_perms_and(ctemp, CHERI_SEALCAP_KERNEL_PERMS);
+	kernel_root_sealcap = ctemp;
+#endif
+
+	ctemp = cheri_address_set(kroot, CHERI_CAP_USER_DATA_BASE);
+	ctemp = cheri_bounds_set(ctemp, CHERI_CAP_USER_DATA_LENGTH);
+	ctemp = cheri_perms_and(ctemp, CHERI_CAP_USER_DATA_PERMS |
+	    CHERI_CAP_USER_CODE_PERMS | CHERI_PERM_SW_VMEM);
+	userspace_root_cap = ctemp;
+
+#ifdef notyet
+	ctemp = cheri_address_set(kroot, CHERI_SEALCAP_USERSPACE_BASE);
+	ctemp = cheri_bounds_set(ctemp, CHERI_SEALCAP_USERSPACE_LENGTH);
+	ctemp = cheri_perms_and(ctemp, CHERI_SEALCAP_USERSPACE_PERMS);
+	userspace_root_sealcap = ctemp;
+#endif
+
+#ifdef notyet
+	ctemp = cheri_address_set(kroot, CHERI_COMPARTMENT_ID_USERSPACE_BASE);
+	ctemp = cheri_bounds_set(ctemp, CHERI_COMPARTMENT_ID_USERSPACE_LENGTH);
+	ctemp = cheri_perms_and(ctemp, CHERI_COMPARTMENT_ID_USERSPACE_PERMS);
+	userspace_root_cidcap = (uintptr_t)ctemp;
+#endif
+
+	ctemp = cheri_address_set(kroot, CHERI_OTYPE_SENTRY);
+	ctemp = cheri_bounds_set(ctemp, 1);
+	ctemp = cheri_perms_and(ctemp, CHERI_PERM_GLOBAL | CHERI_PERM_UNSEAL);
+	sentry_unsealcap = ctemp;
+
+	smccc_ddc_el0 = kroot;
+
+#ifdef notyet
+	swap_restore_cap = kroot;
+#endif
+
+#ifdef notyet
+	vmm_gva_root_cap = kroot;
+
+	vmm_gpa_root_cap = cheri_address_set(kroot, HYP_GPA_MIN_ADDRESS);
+	vmm_gpa_root_cap = cheri_bounds_set(vmm_gpa_root_cap,
+	    HYP_GPA_MAX_ADDRESS - HYP_GPA_MIN_ADDRESS);
+#endif
+
+	ctemp = cheri_address_set(kroot, VM_MAX_KERNEL_ADDRESS -
+	    PMAP_MAPDEV_EARLY_SIZE);
+	ctemp = cheri_bounds_set_exact(ctemp, PMAP_MAPDEV_EARLY_SIZE);
+	ctemp = cheri_perms_and(ctemp, CHERI_PERMS_KERNEL_DATA);
+	devmap_init_capability(ctemp);
+
+	kernel_root_cap = cheri_perms_and(kroot,
+	    ~(CHERI_PERM_SEAL | CHERI_PERM_UNSEAL));
+
+	vmm_el2_root_cap = cheri_address_set(kroot, HYP_VM_MIN_ADDRESS);
+	vmm_el2_root_cap = cheri_bounds_set(vmm_el2_root_cap,
+	    HYP_VM_MAX_ADDRESS - HYP_VM_MIN_ADDRESS);
+}
+
+void
+legacyabi_thread_setregs(struct thread *td, unsigned long entry_addr)
+{
+	struct trapframe *tf;
+
+	tf = td->td_frame;
+
+	/* Set DDC to full user privilege. */
+	tf->tf_ddc = (uintptr_t)cheri_capability_build_user_rwx(
+	    CHERI_CAP_USER_DATA_PERMS | CHERI_PERM_SW_VMEM,
+	    CHERI_CAP_USER_DATA_BASE, CHERI_CAP_USER_DATA_LENGTH,
+	    CHERI_CAP_USER_DATA_OFFSET);
+
+	/* Use 'entry_addr' as offset of PCC. */
+	trapframe_set_elr(tf, (uintptr_t)cheri_capability_build_user_code(
+	    td, CHERI_CAP_USER_CODE_PERMS, CHERI_CAP_USER_CODE_BASE,
+	    CHERI_CAP_USER_CODE_LENGTH, entry_addr));
+}